09f1090c85bcab0219ab4102c0cfbb65545c40d1
Muhannad Abdallah <Muhannadd159@gmail.com>
2024-01-28 14:00:18 +0200

    Add filter class

--- filter.py
+++ filter.py
@@ -1,74 +1,81 @@
-import numpy as np
 from scipy import signal
+import numpy as np
 
-class Filter:
-    def __init__(self):
-        self.zeros = []
-        self.poles = []
-        self.gain = 1
-        # Initialize variables for real-time filtering
-        self.temporal_resolution = 1  # Default temporal resolution (1 point per second)
-        self.filtering_index = 0  # Index indicating the progress of filtering
-
-    def set_filter_components(self, zeros: list, poles: list, gain: float) -> None:
-        """Set the filter components."""
-        self.zeros = zeros
-        self.poles = poles
+class Filter():
+    def __init__(self, poles=None, zeros=None, gain=1):
+        self.poles = poles if poles else []
+        self.zeros = zeros if zeros else []
         self.gain = gain
+        self.all_pass = []
 
-    def get_filter_components(self) -> tuple:
-        """Get the filter components."""
-        return self.zeros, self.poles, self.gain
+    def add_pole(self, pole):
+        self.poles.append(pole)
 
-    def get_frequency_response(self) -> tuple:
-        """Get the frequency response components."""
-        frequency, freq_response = signal.freqz_zpk(self.zeros, self.poles, self.gain)
-        magnitude = 20 * np.log(np.abs(freq_response))
-        phase = np.unwrap(np.angle(freq_response))
-        return frequency, magnitude, phase
+    def add_zero(self, zero):
+        self.zeros.append(zero)
 
-    def apply_filter(self, input_arr: np.ndarray):
-        """Apply the filter to the input signal."""
-        numerator, denominator = signal.zpk2tf(self.zeros, self.poles, self.gain)
-        output_signal = signal.lfilter(numerator, denominator, input_arr)
-        return output_signal.real
+    def add_all_pass(self, all_pass):
+        self.all_pass_append(all_pass)
+
+    def delete_pole(self, pole):
+        """Delete a specific pole from the filter."""
+        if pole in self.poles:
+            self.poles.remove(pole)
+
+    def delete_zero(self, zero):
+        """Delete a specific zero from the filter."""
+        if zero in self.zeros:
+            self.zeros.remove(zero)
+
+    def delete_all_pass(self, all_pass):
+        if all_pass in self.all_pass:
+            self.all_pass.remove(all_pass)
+
+    def delete_poles(self):
+        """Delete all poles from the filter."""
+        self.poles.clear()
+
+    def delete_zeros(self):
+        """Delete all zeros from the filter."""
+        self.zeros.clear()
+
+    def delete_all_passes(self):
+        self.all_pass.clear()
+
+    def delete_all_components(self):
+        self.delete_poles()
+        self.delete_zeros()
+        self.delete_all_passes()
+
+    def get_poles(self):
+        original_poles = [complex(pole.real, pole.imag) for pole in self.poles]
+        all_pass_poles = [all_pass.a for all_pass in self.all_pass]
+        return [*original_poles, *all_pass_poles]
+
+    def get_zeros(self):
+        original_zeros = [complex(zero.real, zero.imag) for zero in self.zeros]
+        all_pass_zeros = [1 / np.conj(all_pass.a) for all_pass in self.all_pass]
+        return [*original_zeros, *all_pass_zeros]
+
+    def get_gain(self):
+        return self.gain
+
+    def get_response(self):
+        zeros_values = self.get_zeros()
+        poles_values = self.get_poles()
+        w, response = signal.freqz_zpk(zeros_values, poles_values, self.gain)
+        magnitude = 20 * np.log10(np.abs(response))
+        phase = np.unwrap(np.angle(response))
+        return w, magnitude, phase
+
+    def get_all_pass_response(self):
+        zeros_values = self.get_zeros()
+        poles_values = self.get_poles()
+        w, response = signal.freqz_zpk(zeros_values, poles_values, self.gain)
+        magnitude = 20 * np.log10(np.abs(response))
+        phase = np.unwrap(np.angle(response))
+        return w, magnitude, phase
     
-    def apply_real_time_filtering(self, input_signal: np.ndarray):
-        """apply the real-time filtering process."""
-        # Determine the number of points to process in each iteration based on temporal resolution
-        points_per_iteration = int(self.temporal_resolution)
 
-        # Calculate the end index for the current iteration
-        end_index = min(self.filtering_index + points_per_iteration, len(input_signal))
 
-        # Get the portion of the signal to process in this iteration
-        signal_chunk = input_signal[self.filtering_index:end_index]
 
-        # Apply the filter to the signal chunk
-        filtered_chunk = self.process.apply_filter(signal_chunk)
-
-        # # Update the graphs with the current chunk of the original and filtered signals
-        # self.update_graphs(signal_chunk, filtered_chunk)
-
-        # Update the filtering index for the next iteration
-        self.filtering_index = end_index
-
-        # Check if filtering is complete
-        if self.filtering_index >= len(input_signal):
-            self.stop_real_time_filtering()
-
-    def start_real_time_filtering(self):
-        # Reset the filtering index
-        self.filtering_index = 0
-
-        # Start a timer to periodically update the real-time filtering process
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_real_time_filtering)
-        self.timer.start(1000)  # Timer interval in milliseconds (adjust as needed)
-
-    def stop_real_time_filtering(self):
-        # Stop the timer when filtering is complete
-        self.timer.stop()
-        self.timer = None
-
-    
